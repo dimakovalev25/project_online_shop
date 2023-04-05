d82fb88fe157f987c281dd704450f51d7806f085
dimakovalev <d.nozdryn@gmail.com>
2023-04-05 14:26:48 +0300

    online shop postgreSQL add

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+
+
+const PORT = process.env.PORT
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({message: 'working!'})
+// })
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
