994fe1b4c4021fca9f9bbf9aa667f2dfa3a98d83
dimakovalev <d.nozdryn@gmail.com>
2023-04-06 15:34:22 +0300

    start front

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const models = require('./models/models')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
 
 const router = require('./routes/index')
 
@@ -13,6 +15,7 @@ const PORT = process.env.PORT
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+//декодировать токен и проверка на валидность
+
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next){
+    if (req.method === 'OPTIONS') {
+        next()
+    }
+
+    try {
+
+        const token = req.headers.authorization.split(' ')[1]
+
+        if(!token){
+          return   res.status(401).json({mess: 'not authorized'})
+        }
+
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+
+        req.user = decoded
+        next()
+
+    } catch (e) {
+        res.status(401).json({mess: 'not authorized'})
+    }
+}
